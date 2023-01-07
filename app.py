47eb310bb4d821a491fab72c1b3f81963fbdbc52
Taras Svirskyi <tsvirskyi@gocardless.com>
2023-01-07 22:13:10 +0000

    added cache and example of progressbar

--- app.py
+++ app.py
@@ -6,12 +6,18 @@ import numpy as np
 st.text('Hello world ++!')
 phello = st.sidebar.checkbox('Personalised greetings')
 
+# generating data in cahced funciton ensures that they are the same when users
+# interacts with app
+@st.cache
+def generate_rand_data():
+    x = np.arange(0,10)
+    df = pd.DataFrame({"x":x, 
+                    "y2": [i*i for i in x], 
+                    "y3": [i**2.5 for i in x],
+                    "word": np.random.choice(['a','bb','ccc'], size=len(x))})
+    return df
 
-x = np.arange(0,10)
-df = pd.DataFrame({"x":x, 
-                  "y2": [i*i for i in x], 
-                  "y3": [i**2.5 for i in x],
-                  "word": np.random.choice(['a','bb','ccc'], size=len(x))})
+df = generate_rand_data()
 st.line_chart(df,x='x', y=['y2','y3'])
 
 #st.write(df.head(3))
@@ -23,15 +29,19 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 st.line_chart(chart_data)
 
-if st.checkbox('Show me the map'):
-    map_data = pd.DataFrame(
+
+@st.cache
+def generate_map_data():
+    return pd.DataFrame(
         np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
         columns=['lat', 'lon'])
 
+if st.checkbox('Show me the map'):
+    map_data = generate_map_data()
     st.map(map_data)
 
 ### widgets
-x = st.slider('x')  # 👈 this is a widget
+x = st.slider('x',0,20,3)  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
 
 
@@ -56,3 +66,27 @@ tcat = st.selectbox("choose category:", categories.keys())
 tsubcat = st.selectbox("choose subcategory:", categories[tcat])
 st.write(f"Your tcats: {tcat}/{tsubcat}")
 
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+b = left_column.button('Press me!')
+st.write(b)
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
